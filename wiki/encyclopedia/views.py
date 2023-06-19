19adec59854a5546796763ef5f9346ee0472a080
abdelkareem <26a.abood@gmail.com>
2023-06-19 20:19:54 +0300

    Implement search bar

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -12,3 +12,16 @@ def wiki(request, title):
     return render(request, "encyclopedia/wiki.html", {
         "entry": util.get_entry(title)
     })
+
+def search(request):
+    entry = request.POST.get('q')
+    entries = util.list_entries()
+    possibilities = []
+    for element in entries:
+        for i in element:
+            if i in entry:
+                possibilities.append(element)
+    return render(request, "encyclopedia/search.html", {
+        "possibilities": possibilities,
+        "entry": util.get_entry(entry)
+    })
