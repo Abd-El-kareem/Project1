5c480a702452ddebe4041c1a9d5729ae5773b995
abdelkareem <26a.abood@gmail.com>
2023-06-19 22:25:39 +0300

    Implement Creat A New Page

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect, HttpResponse
+
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="Title")
+    content = forms.CharField(widget=forms.Textarea(attrs={'name':'body', 'rows':3, 'cols':5, 'style': 'height: 15em;'}))
 
 from . import util
 
@@ -19,9 +25,25 @@ def search(request):
     possibilities = []
     for element in entries:
         for i in element:
-            if i in entry:
-                possibilities.append(element)
+            if i.lower() in entry.lower():
+                if element not in possibilities:
+                    possibilities.append(element)
     return render(request, "encyclopedia/search.html", {
         "possibilities": possibilities,
         "entry": util.get_entry(entry)
+    })
+
+def create(request):
+    if request.method == "POST":
+         form = NewEntryForm(request.POST)
+         if form.is_valid():
+              title = form.cleaned_data["title"]
+              content = form.cleaned_data["content"]
+              if title not in util.list_entries():
+                  util.save_entry(title, content)
+                  return HttpResponseRedirect(f'/wiki/{title}')
+              else:
+                  return render(request, "encyclopedia/error.html")
+    return render(request, "encyclopedia/create.html", {
+        "form": NewEntryForm()
     })
